5c345345a64d42402c2a15209673856dc72bd2f8
Anthony shadowitz <anthonyshadowitz@Anthonys-MacBook-Pro.local>
2022-07-10 11:33:26 -0400

    create dummy info and make sure it is displaying properly when passed down to children

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,19 @@
 import React from "react";
 
+import UsersList from "../components/UserList";
+
 const Users = () => {
-  return <h2>Users</h2>;
+  const USERS = [
+    {
+      id: "ui",
+      name: "Anthony Shadowitz",
+      image:
+        "https://i.kym-cdn.com/entries/icons/original/000/018/385/Rs_634x1024-130605092844-634.DespMe2.mh.060513.jpg",
+      places: 3,
+    },
+  ];
+
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
