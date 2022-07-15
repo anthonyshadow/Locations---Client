a266470de3d4f72304c9955820857eaabecad23c
Anthony shadowitz <anthonyshadowitz@Anthonys-MacBook-Pro.local>
2022-07-15 14:31:14 -0400

    add dummy places for users, and custom buttons

--- src/App.js
+++ src/App.js
@@ -9,21 +9,25 @@ import {
 import MainNavigation from "./shared/navigation/MainNavigation";
 import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
+import UserPlaces from "./places/pages/UserPlaces";
 
 function App() {
   return (
     <Router>
-      <MainNavigation/>
+      <MainNavigation />
       <main>
-      <Switch>
-        <Route path="/" exact>
-          <Users />
-        </Route>
-        <Route path="/places/new" exact>
-          <NewPlace />
-        </Route>
-        <Redirect to="/" />
-      </Switch>
+        <Switch>
+          <Route path="/" exact>
+            <Users />
+          </Route>
+          <Route path="/:userId/places" exact>
+            <UserPlaces />
+          </Route>
+          <Route path="/places/new" exact>
+            <NewPlace />
+          </Route>
+          <Redirect to="/" />
+        </Switch>
       </main>
     </Router>
   );
--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -5,7 +5,7 @@ import UsersList from "../components/UserList";
 const Users = () => {
   const USERS = [
     {
-      id: "ui",
+      id: "u1",
       name: "Anthony Shadowitz",
       image:
         "https://i.kym-cdn.com/entries/icons/original/000/018/385/Rs_634x1024-130605092844-634.DespMe2.mh.060513.jpg",
